18ee9814dc77ed7e019287d2704d06377491386f
Jojodob74 <joris@guyonnet.ch>
2026-03-28 18:40:51 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://clement-langues.netlify.app',
+  site: 'https://comforting-mooncake-1137c0.netlify.app',
 });
